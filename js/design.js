6c9a7aabf37bf8c4016d0dca3b88bde8f12559d3
M7UNDO <dhlaminimfundo1@gmail.com>
2025-05-18 19:16:55 +0200

    Wireframe Carousel 1

---
+++ js/design.js
@@ -0,0 +1,66 @@
+/*let wireFrameIndex = 1;
+showSlides(wireFrameIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(wireFrameIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(wireFrameIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("initial-wireframe");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length){wireFrameIndex = 1}
+  if(n < 1) {wireFrameIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" activeDot", "");
+  }
+  slides[wireFrameIndex-1].style.display = "flex";
+  dots[wireFrameIndex-1].className += " activeDot";
+}
+*/
+
+let wireFrameIndex = 1; //tracking what slide is currently being displayed, setting it to 1 by default
+showSlides(wireFrameIndex); //Calling the function display the first slide immediately when my pages loads
+
+
+function plusSlides(n) {
+  showSlides(wireFrameIndex += n); //when button is pressed wireframe equals the next/previous in the index essentially wireframe + n = wireframe or wireframe + -n
+}
+
+function currentSlide(n) {
+  showSlides(wireFrameIndex = n); //Triggered when clicking on a dot, essentia setting wireframe index to the current dot number
+}
+
+function showSlides(n) {
+  let i; //Variable i is declared to use in the following loops
+  let slides = document.getElementsByClassName("initial-wireframe");
+  let dot = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    wireFrameIndex = 1; //Simply put it jumps to one if we go to the last slide(5) and press next but there is no next...
+  }
+
+  if (n < 1) {
+    wireFrameIndex = slides.length; //Jumps to the last slide if we press previous when on the first slide, in this case slides.length = 5
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    //*note to self: its semi colons not commas in a for loop
+    slides[i].style.display = "none"; //Loop: Hiding all the slides before I pick one
+  }
+
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" activeDot", ""); //Remmoves the activeDot class name to clear the selction and give its original className;
+  }
+  //Doing this because Arrays start at 0;
+  slides[wireFrameIndex - 1].style.display = "flex"; //Displays the selected slide (wireFrameIndex - 1, because arrays are 0-based).
+  dot[wireFrameIndex - 1].className += " activeDot"; //Adds the "activeDot" class to the corresponding dot to highlight it.
+}

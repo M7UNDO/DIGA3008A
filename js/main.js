7eba2c89051f5eb264d47c476f958947b5dac0aa
M7UNDO <dhlaminimfundo1@gmail.com>
2025-05-23 23:43:33 +0200

    Subheading effect Issue Fixed

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-const root = document.documentElement
-
-const siteColour1 = getComputedStyle(root).getPropertyValue('--site-color-1');
-const siteColour2 = getComputedStyle(root).getPropertyValue('--site-color-2');
-const siteColour3 = getComputedStyle(root).getPropertyValue('--site-color-3');
-
-let menuButton = document.querySelector(".menuButton");
-
-//menuButton.style.transition = "background-color 0.3s ease, color 0.3s ease";
-menuButton.style.transition = "all 0.5s ease 0s";
-
-menuButton.addEventListener("mouseover", function(){
-
-   menuButton.style.backgroundColor = siteColour1;
-   menuButton.style.color = "white";
-
-})
-
-menuButton.addEventListener("mouseout", function(){
-
-   menuButton.style.backgroundColor = "";
-   menuButton.style.color = "";
-
-})
-
 
 console.log("main.js loaded");
 let mybutton = document.getElementById("myBtn");

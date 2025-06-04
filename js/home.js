2c97935a293a3700709fab646ec4d29f7d56d68c
M7UNDO <dhlaminimfundo1@gmail.com>
2025-06-05 01:10:10 +0200

    API Added

--- js/home.js
+++ js/home.js
@@ -28,3 +28,20 @@ window.addEventListener("load", function(){
   document.getElementById("heading-intro").classList.add("slide-in");
 });
 
+
+const quote = document.getElementById("quote")
+const author = document.getElementById("author")
+const authorImage = document.getElementById("author-image")
+const api_url = "https://api.quotable.io/random"
+
+async function getQuote(url){
+
+   const response = await fetch(url);
+   var data = await response.json();
+   console.log(data)
+   
+   quote.innerHTML = data.content;
+   author.innerHTML = "~ "  + data.author;
+}
+
+getQuote(api_url);
